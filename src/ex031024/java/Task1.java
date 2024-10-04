c7f0543cae7564d3c074aca571551aa88fabaae6
Kavitta79 <kavitayd79@gmail.com>
2024-10-05 02:23:43 +0530

    Update Task1.java

--- src/ex031024/java/Task1.java
+++ src/ex031024/java/Task1.java
@@ -1,4 +1,18 @@
 package ex031024.java;
 
 public class Task1 {
+  public static void main(String[] args){
+        System.out.println("Simple Calculator");
+        System.out.println("Addition of two numbers is: ");
+        System.out.println(25+35);
+
+        System.out.println("Subtraction of two numbers is: ");
+        System.out.println(50-25);
+
+        System.out.println("Multiplication of two numbers is: ");
+        System.out.println(67*12);
+
+        System.out.println("Division of two number is ");
+        System.out.println(49/7);
+    }
 }

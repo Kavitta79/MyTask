42c4190ecb6f400002cfcd6c122123a0bc5b489e
kavita-1111 <kavitayd1111@gmail.com>
2024-10-14 01:10:48 +0530

    Adding Task 4 and Task 5

--- src/ex031024/java/Task5.java
+++ src/ex031024/java/Task5.java
@@ -1,4 +1,11 @@
 package ex031024.java;
 
 public class Task5 {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a % b = " + (a % b));
+    }
 }
